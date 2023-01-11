3457a0507061431d54cba2e04d082828b6980178
carter zimmerman <carterzimm@yahoo.com>
2023-01-11 11:41:07 -0700

    started project

--- flasher9000/ViewController.swift
+++ flasher9000/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
-
-
+    
+    
+    @IBAction func flashbutton(_ sender: Any) {
+        onButtonTap(_ sender: UIButton) {
+            self.view.backroundColor = UIColor.Blue
+        }
+        
+        
+    }
 }
-
